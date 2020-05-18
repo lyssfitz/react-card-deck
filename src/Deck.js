1d76a5751da4691790f192c8bdc341c5aeca7876
Lyss <lyssfitz@outlook.com>
2020-05-18 12:18:26 +1000

    Add styling to make cards show up at random angles on Deck.

--- src/Deck.js
+++ src/Deck.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Card from './Card';
 
+import './Deck.css';
+
 const API_BASE_URL = 'https://deckofcardsapi.com/api/deck/';
 
 class Deck extends Component {
@@ -44,7 +46,7 @@ class Deck extends Component {
 			<div>
 				<h1>Card Dealer</h1>
 				<button onClick={this.getCard}>Get Card!</button>
-				{cards}
+				<div className="Deck-cardarea">{cards}</div>
 			</div>
 		);
 	}
